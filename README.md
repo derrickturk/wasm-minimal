8767c0494b7feb089a76434a8005130ebba28ebe
Derrick W. Turk <dwt@terminusdatascience.com>
2020-12-03 18:10:03 -0600

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust WASM without wasm-pack
+
+Make sure to `cargo install wasm-bindgen-cli` (it's hard to find...) first.
--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import init, { hello } from './wasm_minimal.js';
 async function run()
 {
     await init();
-    hello('bitches');
+    hello('without wasm-pack');
 }
 
 run();
